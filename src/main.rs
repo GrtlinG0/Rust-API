c155f9acdb3fb3387a2bfb3c5df0571dd4433894
GrtlinG0 <adamkural1@gmail.com>
2023-09-19 16:51:22 +0200

    0.1

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ fn app() -> Router {
 }
 
 async fn handler() -> &'static str {
-    "Let's Get Rusty!"
+    "TOHLE JE FRONTEND"
 }
 
 async fn get_users(state: Extension<Pool<Postgres>>) -> Json<Vec<User>> {
